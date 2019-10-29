b2815c00b5a25584dd68c7f4f24802875436bc08
FonsecaCarlos <foncecacarlos@hotmail.com>
2019-10-28 21:57:49 -0400

    Updates

---
+++ precache-manifest.7f498b306c3ac29da616b5fb4351d85c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "453f4c90bea6e39f810b35987681cef7",
+    "url": "/SWAPI-Frontend/index.html"
+  },
+  {
+    "revision": "31656f8d3d3e48091122",
+    "url": "/SWAPI-Frontend/static/css/main.3093c623.chunk.css"
+  },
+  {
+    "revision": "c433060cbbed83368063",
+    "url": "/SWAPI-Frontend/static/js/2.43dd9522.chunk.js"
+  },
+  {
+    "revision": "31656f8d3d3e48091122",
+    "url": "/SWAPI-Frontend/static/js/main.799fc920.chunk.js"
+  },
+  {
+    "revision": "b186e6e023b4c24c437f",
+    "url": "/SWAPI-Frontend/static/js/runtime~main.179df7ae.js"
+  }
+]);
